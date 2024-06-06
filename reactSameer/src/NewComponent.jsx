b260e6a539baeb581fc049938a274c0ce2ca7ed8
chetnathukral <chetnathukral06@gmail.com>
2024-06-05 21:23:02 -0400

    Added NewComponent to the project

---
+++ reactSameer/src/NewComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NewComponent = () => {
+    return (
+        <div>
+            <h1>This is a new component</h1>
+        </div>
+    );
+}
+
+export default NewComponent;
